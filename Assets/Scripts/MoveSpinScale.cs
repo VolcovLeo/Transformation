a68fd6dbf72a244ab08c9613faea579c17df55cc
VolcovLeo <volcov8leo@gmail.com>
2026-04-24 15:03:02 +0300

    Init

---
+++ Assets/Scripts/MoveSpinScale.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _scaleSpeed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+
+        transform.Rotate(0f, _rotateSpeed * Time.deltaTime, 0f);
+
+        transform.localScale += Vector3.one * _scaleSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Resize.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resize : MonoBehaviour
+{
+    [SerializeField] private float _scaleSpeed;
+
+
+    void Update()
+    {
+        transform.localScale += Vector3.one * _scaleSpeed * Time.deltaTime;
+    }
+}
